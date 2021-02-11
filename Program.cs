708f74d16354d52b229856dcce2427e8c67c1d13
JPedro Brito <jpedro0brito@gmail.com>
2021-02-11 19:24:15 -0400

    [Novo] adicionando o Projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.ConsoleApp.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
